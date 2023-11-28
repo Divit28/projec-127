a5dfeffc99f0cbe524e30975183281aa843e1460
Divit28 <90520758+Divit28@users.noreply.github.com>
2023-11-28 15:07:57 +0530

    U main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ Peter_pan_song="";
 Harry_potter_theme_song="";
 
 function setup(){
-    canvas = createCanvas(600,530);
+    canvas = createCanvas(500,530);
     canvas.center();
 
     video = createCapture(VIDEO);
@@ -15,5 +15,5 @@ function preload(){
 }
 
 function draw(){
-    image(video,0,0,600,530);
+    image(video,0,0,500,530);
 }
